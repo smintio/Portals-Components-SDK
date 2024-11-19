bd09a38e04d1427779086e90e899825f252d4d14
Reinhard Holzner <reinhard@smint.io>
2024-11-19 15:34:01 +0100

    Correctly order SharePoint configuration

--- Examples/Backend/Connectors/Connector-SharePoint/SharepointOneDriveConnectorConfiguration.cs
+++ Examples/Backend/Connectors/Connector-SharePoint/SharepointOneDriveConnectorConfiguration.cs
@@ -8,25 +8,25 @@ namespace SmintIo.Portals.Connector.SharePoint
 
         bool HighSecurityMode { get; set; }
 
-        string SiteId { get; set; }
-
-        string SiteIdString { get; set; }
-
-        string SiteDriveId { get; set; }
-
-        string SiteListId { get; set; }
-
-        string[] SiteFolderIds { get; set; }
-
         string TenantId { get; set; }
 
         string ClientId { get; set; }
 
         string ClientSecret { get; set; }
 
+        string SiteId { get; set; }
+
+        string SiteIdString { get; set; }
+
+        string SiteDriveId { get; set; }
+
         string SiteDriveIdString { get; set; }
 
+        string SiteListId { get; set; }
+
         string SiteListIdString { get; set; }
+
+        string[] SiteFolderIds { get; set; }
 
         string[] SiteFolderIdsStrings { get; set; }
     }
